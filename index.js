1f661e30d7592cfcabce6502630e8ff1074b8a29
reaganongaya <reagan.ongaya@student.moringaschool.com>
2022-12-16 15:12:39 +0300

    Assignment Completed

---
+++ index.js
@@ -0,0 +1,11 @@
+
+function insertImage() {
+    document.querySelectorAll('.box').forEach(image=> {
+         
+        if(image.innerText.length !== 0) {
+            if(image.innerText == 'Wpawn' || image.innerText == 'Bpawn') {
+                image.innerHTML = 
+            }
+        }
+    })
+}
